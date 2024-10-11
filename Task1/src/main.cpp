11aa85e923dd2b59e089d2871af6dea04ae66e89
Михаил Беляков <misa.belyakov.ru@gmail.com>
2024-10-12 01:13:42 +0700

    Task 1 done

--- Task1/src/main.cpp
+++ Task1/src/main.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
+using namespace std;
 
-int main(int argc, char *argv[])
-{
-    std::cout << "Hello world!" << std::endl;
+string encrypt_caesar(string text, int code){
+    for(int i = 0; i < text.length(); i++){
+        if(!isalpha(text[i])){
+            continue;
+        }else if(text[i] >= 'A' && text[i] <= 'Z'){
+            text[i] = ((text[i] - 'A' + code) % 26) + 'A';
+        }else if(text[i] >= 'a' && text[i] <= 'z'){
+            text[i] = ((text[i] - 'a' + code) % 26) + 'a';
+        }
+    }
+    return text;
+}
+
+// Almoust working :(
+
+// string decrypt_caesar(string text, int code){
+//     for (int i = 0; i < text.length(); i++) {
+//         if (!isalpha(text[i])) {
+//             continue;
+//         } else if (text[i] >= 'A' && text[i] <= 'Z') {
+//             text[i] = ((text[i] - 'A' - code + 26) % 26) + 'A';
+//         } else if (text[i] >= 'a' && text[i] <= 'z') {
+//             text[i] = ((text[i] - 'a' - code + 26) % 26) + 'a';
+//         }
+//     }
+//     return text;
+// }
+
+
+
+int main(){
+
+    string text = "The quick brown fox jumps over the lazy dog*";
+    int code = 29;
+    cout << encrypt_caesar(text, code) << endl;
+
+    cout << decrypt_caesar(encrypt_caesar(text, code), code) << endl;
 }
