540e6217d1dccead565732b5a248b96e4725bf09
M1SH4-cmd <6el0zu61979@mail.ru>
2024-10-13 20:17:06 +0700

    Working, still to test

--- Task2/src/main.cpp
+++ Task2/src/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <regex>
+using namespace std;
 
-int main(int argc, char *argv[])
-{
-    std::cout << "Hello world!" << std::endl;
+int main(){
+    cout << "Enter your email for validation:" << endl;
+    string email;
+    cin >> email;
+
+    regex r ("(\\w+)(\\.|_)?(\\w*)@(\\w+)(\\.(\\w+))+");
+    regex_match(email, r) ? cout << "Email is valid" << endl : cout << "Email is not valid" << endl;
 }
