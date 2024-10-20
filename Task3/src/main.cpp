3810cf1c8be18d03bd20f0a387ee935b13ca9774
Михаил Беляков <misa.belyakov.ru@gmail.com>
2024-10-21 01:06:15 +0700

    Alles sind Arbeiten

--- Task3/src/main.cpp
+++ Task3/src/main.cpp
@@ -1,13 +1,49 @@
 #include <iostream>
-#include <regex>
+#include <string>
+
 using namespace std;
 
-int main(){
-    cout << "Enter your IP address for validation:" << endl;
+bool isValidIP(string ip) {
+    int num = 0;
+    int dotCount = 0;
+    int length = ip.length();
+
+    for (int i = 0; i < length; i++) {
+        char c = ip[i];
+
+        if (c == '.') {
+            if (num < 0 || num > 255 || (i > 0 && ip[i - 1] == '.')) {
+                return false;
+            }
+            dotCount++;
+            num = -1;
+        } else if (c >= '0' && c <= '9') {
+            if (num == -1) {
+                num = 0;
+            }
+            num = num * 10 + (c - '0');
+        } else {
+            return false;
+        }
+    }
+
+    if (num < 0 || num > 255 || dotCount != 3) {
+        return false;
+    }
+
+    return true;
+}
+
+int main() {
     string ip;
+    cout << "Введите IP-адрес для проверки: ";
     cin >> ip;
 
-    regex r("^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$");
+    if (isValidIP(ip)) {
+        cout << "Valid" << endl;
+    } else {
+        cout << "Invalid" << endl;
+    }
 
-    regex_match(ip, r) ? cout << "IP is valid" << endl : cout << "Invalid IP" << endl;
+    return 0;
 }
