8c2ca8a3248173ef6fface295e6a7235eb03d230
Михаил Беляков <misa.belyakov.ru@gmail.com>
2024-10-20 23:06:55 +0700

    Reworked, all working

--- Task2/src/main.cpp
+++ Task2/src/main.cpp
@@ -1,72 +1,104 @@
 #include <iostream>
 using namespace std;
 
-bool isFirstPartValid(string firstPart){
-    for(int i = 0; i < firstPart.length(); i++){
-        if((firstPart[i] < 'A' && firstPart[i] > 'Z') || (firstPart[i] < 'a' && firstPart[i] > 'z') || (firstPart[i] != '.' || firstPart[i] != '-' || firstPart[i] != '_')){
-            return false;
-        }else if(firstPart[i] == '.' && firstPart[i + 1] == '.'){
-                return false;
-        }else if(firstPart[i] == '@' && firstPart[i + 1] == '@'){
+bool isFirstPartValid(string firstPart) {
+    if (firstPart.length() < 1 || firstPart.length() > 64) return false;
+
+    bool lastWasDot = false;
+
+    for (int i = 0; i < firstPart.length(); i++) {
+        char c = firstPart[i];
+
+        if (!((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || 
+              (c >= '0' && c <= '9') || c == '.' || c == '-' || 
+              c == '!' || c == '#' || c == '$' || c == '%' || 
+              c == '&' || c == '\'' || c == '*' || c == '+' || 
+              c == '/' || c == '=' || c == '?' || c == '^' || 
+              c == '_' || c == '`' || c == '{' || c == '|' || c == '}' || c == '~')) {
             return false;
         }
+
+        if (c == '.') {
+            if (lastWasDot) {
+                return false;
+            }
+            lastWasDot = true;
+        } else {
+            lastWasDot = false;
+        }
     }
+
+    if (firstPart.front() == '.' || firstPart.back() == '.') {
+        return false;
+    }
+
     return true;
 }
 
+bool isSecondPartValid(string secondPart) {
+    if (secondPart.length() < 1 || secondPart.length() > 63) return false;
 
-bool isSecondPartValid(string secondPart){
-    for(int i = 0; i < secondPart.length(); i++){
-        if((secondPart[i] < 'a' && secondPart[i] > 'z') || (secondPart[i] != '.' || secondPart[i] != '-' || secondPart[i] != '_')){
+    bool lastWasDot = false; 
+
+    for (int i = 0; i < secondPart.length(); i++) {
+        char c = secondPart[i];
+
+        if (!((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || 
+              (c >= '0' && c <= '9') || c == '.' || c == '-')) {
             return false;
-        }else if(secondPart[i] == '.' && secondPart[i + 1] == '.'){
+        }
+
+        if (c == '.') {
+            if (lastWasDot) {
                 return false;
+            }
+            lastWasDot = true;
+        } else {
+            lastWasDot = false;
         }
     }
+
+    if (secondPart.front() == '.' || secondPart.back() == '.') {
+        return false;
+    }
+
     return true;
 }
-
-
-
 
 bool isValidEmail(string email) {
-    int dogPos = email.find('@');
-    if(email.find('@', dogPos)){
+    int dogPos = -1;
+
+    
+    for (int i = 0; i < email.length(); i++) {
+        if (email[i] == '@') {
+            if (dogPos != -1) {
+                return false;
+            }
+            dogPos = i;
+        }
+    }
+
+    
+    if (dogPos == -1 || dogPos == 0 || dogPos == email.length() - 1) {
         return false;
     }
+
     string firstPart = email.substr(0, dogPos);
-    string secondPart = email.substr(dogPos, email.length());
-    int frstLen = firstPart.length();
+    string secondPart = email.substr(dogPos + 1); 
 
-
-    if(frstLen > 64){
-        return false;
-    }
-
-    bool valid = isFirstPartValid(firstPart) && isSecondPartValid(secondPart);
-    if(valid){
-        return true;
-    }
-    return false;
+    return isFirstPartValid(firstPart) && isSecondPartValid(secondPart);
 }
-
-
-
-
-
-
-
 
 int main() {
     string email;
-    cout << "Enter your email for validation:" << endl;
+    cout << "Enter email for validation:" << endl;
     cin >> email;
-    
+
     if (isValidEmail(email)) {
         cout << "Yes" << endl;
     } else {
         cout << "No" << endl;
     }
-    
+
     return 0;
 }
